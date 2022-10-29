93d98f10bcaeb34ea78830c52206b92b6f753690
mdabusayedislam <abusayediit@gmail.com>
2022-10-29 20:36:31 +0600

    Add project files.

---
+++ WebAppWithStoredProccedure/Data/DbContextClass.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppWithStoredProccedure.Entities;
+
+namespace WebAppWithStoredProccedure.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ WebAppWithStoredProccedure/Repositories/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using WebAppWithStoredProccedure.Entities;
+
+namespace WebAppWithStoredProccedure.Repositories
+{
+    public interface IProductService
+    {
+        public Task<List<Product>> GetProductListAsync();
+        public Task<IEnumerable<Product>> GetProductByIdAsync(int Id);
+        public Task<int> AddProductAsync(Product product);
+        public Task<int> UpdateProductAsync(Product product);
+        public Task<int> DeleteProductAsync(int Id);
+    }
+}
